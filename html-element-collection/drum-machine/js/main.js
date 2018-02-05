5df28549dd2bf6dae11c401532f2f1a4ec0a2611
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-02-05 19:53:15 +0300

    drum-machine is corrected. Can play any sound again by putting the play-button before this sound ends.

--- html-element-collection/drum-machine/js/main.js
+++ html-element-collection/drum-machine/js/main.js
@@ -2,11 +2,12 @@
 
 const drums = document.getElementsByClassName('drum-kit__drum');
 
-for (const drum of drums) {
-    drum.onclick = drumPlay;
-}
-
 function drumPlay() {
     const audioNode = this.getElementsByTagName('audio')[0];
+    audioNode.currentTime = 0;
     audioNode.play();
+}
+
+for (const drum of drums) {
+    drum.onclick = drumPlay;
 }
