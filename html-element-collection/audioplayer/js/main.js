6178461cbc40d05437856e418b546700c28fd547
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-02-05 20:43:56 +0300

    HJ 1.2. Audioplayer is corrected. Use global hrefs from the task description unstead of local hrefs

--- html-element-collection/audioplayer/js/main.js
+++ html-element-collection/audioplayer/js/main.js
@@ -12,14 +12,18 @@ const songTitleTag = mediaplayer.getElementsByClassName('title')[0];
 const audioTag = mediaplayer.getElementsByTagName('audio')[0];
 
 const audioSources = [
-    './mp3/LA Chill Tour.mp3',
-    './mp3/This is it band.mp3',
-    './mp3/LA Fusion Jam.mp3'
+    'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA Chill Tour.mp3',
+    'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/This is it band.mp3',
+    'https://netology-code.github.io/hj-homeworks/html-element-collection/audioplayer/mp3/LA Fusion Jam.mp3'
 ];
 audioSources.currIndex = 0;
 
+function getSongNameFromSongHref(href) {
+    return href.match(/\/([\w\s]+)\.mp3$/)[1];
+}
+
 audioTag.src = audioSources[audioSources.currIndex];
-songTitleTag.title = audioSources[audioSources.currIndex].slice(6, -4);
+songTitleTag.title = getSongNameFromSongHref( audioSources[audioSources.currIndex] );
 
 
 playstateButton.onclick = () => {
@@ -49,7 +53,7 @@ backButton.onclick = () => {
     const sourcesNumber = audioSources.length;
     audioSources.currIndex = (audioSources.currIndex + sourcesNumber - 1) % sourcesNumber;
     audioTag.src = audioSources[audioSources.currIndex];
-    songTitleTag.title = audioSources[audioSources.currIndex].slice(6, -4);
+    songTitleTag.title = getSongNameFromSongHref( audioSources[audioSources.currIndex] );
 
     //включает проигрывание, если оно было включено в момент нажатия кнопки смены песни
     if (isPlaying) playstateButton.onclick();
@@ -63,7 +67,7 @@ nextButton.onclick = () => {
     const sourcesNumber = audioSources.length;
     audioSources.currIndex = (audioSources.currIndex + 1) % sourcesNumber;
     audioTag.src = audioSources[audioSources.currIndex];
-    songTitleTag.title = audioSources[audioSources.currIndex].slice(6, -4);
+    songTitleTag.title = getSongNameFromSongHref( audioSources[audioSources.currIndex] );
 
     //включает проигрывание, если оно было включено в момент нажатия кнопки смены песни
     if (isPlaying) playstateButton.onclick();
