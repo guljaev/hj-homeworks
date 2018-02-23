ea4a33f9c5b62f9a771123e2bcfc5abb77818621
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-02-23 14:53:01 +0300

    HJ 2.2 contact-list is corrected

--- dom/contact-list/js/main.js
+++ dom/contact-list/js/main.js
@@ -3,15 +3,23 @@
 const contactsUlNode = document.querySelector('.contacts-list');
 const contacts = JSON.parse(loadContacts());
 
-contactsUlNode.innerHTML = '';
+// contactsUlNode.innerHTML = '';
+// for (let i = 0; i < contacts.length; i++) {
+//     contactsUlNode.innerHTML += '<li><strong></strong></li>';
+// }
+
+// const contactsLiNodes = contactsUlNode.getElementsByTagName('li');
+// Array.from(contactsLiNodes).forEach((liNode, i) => {
+//     liNode.dataset.email = contacts[i].email;
+//     liNode.dataset.phone = contacts[i].phone;
+//     liNode.querySelector('strong').innerHTML = contacts[i].name;
+// });
+
+let ulNodeContent = '';
 for (let i = 0; i < contacts.length; i++) {
-    contactsUlNode.innerHTML += '<li><strong></strong></li>';
+    ulNodeContent += `<li data-email=${contacts[i].email} data-phone="${contacts[i].phone}">
+    <strong>${contacts[i].name}</strong>
+    </li>`;
 }
 
-const contactsLiNodes = contactsUlNode.getElementsByTagName('li');
-Array.from(contactsLiNodes).forEach((liNode, i) => {
-    liNode.dataset.email = contacts[i].email;
-    liNode.dataset.phone = contacts[i].phone;
-    liNode.querySelector('strong').innerHTML = contacts[i].name;
-});
-
+contactsUlNode.innerHTML = ulNodeContent;
