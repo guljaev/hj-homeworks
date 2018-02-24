69d155bffa9087fd30641c2ce21bda84aab89f3c
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-02-24 20:44:55 +0300

    HJ 3.2 table is done

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,25 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    let target = event.target;
+    const table = event.currentTarget;
+
+    // if (target.tagName !== 'TH') {
+    //     return;
+    // }
+    // target.dataset.dir === '1' ? target.dataset.dir = '-1' : target.dataset.dir = '1';
+    // table.dataset.sortBy = target.dataset.propName;
+    // sortTable(target.dataset.propName, Number(target.dataset.dir));
+
+    // ниже решение, предусматривающее возможность включения дополнительных тегов в ячейки th с развитием сайта
+    while (target !== table) {
+        if (target.tagName === 'TH') {
+            target.dataset.dir === '1' ? target.dataset.dir = '-1' : target.dataset.dir = '1';
+            table.dataset.sortBy = target.dataset.propName;
+            sortTable(target.dataset.propName, Number(target.dataset.dir));
+            break;
+        }
+        target = target.parentElement;
+        console.log(target.tagName);
+    }
 }
