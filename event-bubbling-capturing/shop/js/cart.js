1ba9ec0fe6ce4e89046ad4d406444f8525fada92
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-02-24 21:59:57 +0300

    HJ 3.2 shop is done

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function updateAddToCartButtonsFunctions() {
+    const buttons = document.querySelectorAll('.add-to-cart');
+    Array.from(buttons).forEach(button => {
+        button.addEventListener('click', addToCartButtonFunction);
+    });
+
+    function addToCartButtonFunction(event) {
+        event.preventDefault();
+        const title = event.currentTarget.dataset.title;
+        const price = event.currentTarget.dataset.price;
+        const item = items
+            .filter(item => ( (item.title === title) && (item.price == price) ))
+            .pop();
+        addToCart(item, title, price);
+    }
+}
+
+showMore.addEventListener('click', () => {
+    updateAddToCartButtonsFunctions();
+});
+
+updateAddToCartButtonsFunctions();
