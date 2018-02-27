8fcace3fd707ad33b8a10a9c23600e8efced724e
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-02-27 22:30:02 +0300

    HJ 3.3 counter is done

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const counter = document.getElementById('counter');
+const increment = document.getElementById('increment');
+const decrement = document.getElementById('decrement');
+const reset = document.getElementById('reset');
+
+if (!localStorage.counter) {
+    localStorage.counter = 0;
+}
+counter.textContent = localStorage.counter;
+
+increment.addEventListener('click', () => {
+    localStorage.counter++;
+    counter.textContent = localStorage.counter;
+});
+
+decrement.addEventListener('click', () => {
+    localStorage.counter--;
+    counter.textContent = localStorage.counter;
+});
+
+reset.addEventListener('click', () => {
+    localStorage.counter = 0;
+    counter.textContent = localStorage.counter;
+});
