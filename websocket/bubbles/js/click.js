98e68b1bca82662d8167d799f8e19e79118a1ab0
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-03-03 22:02:19 +0300

    HJ 3.4 bubbles is done

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,14 @@
 'use strinct';
+
+const socket = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+showBubbles(socket);
+
+document.querySelector('body').addEventListener('click', event => {
+    const coords = {
+        x: event.clientX,
+        y: event.clientY
+    };
+    // console.log(coords);
+    socket.send(JSON.stringify(coords));
+    showBubbles(socket);
+})
