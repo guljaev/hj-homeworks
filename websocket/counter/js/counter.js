3c1ec8a55d7f14c20535d741005d295921dac37e
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-03-04 14:38:04 +0300

    HJ 3.4 counter is done

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,33 @@
 'use strict';
+const socket = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+socket.addEventListener('open', () => {
+    console.log('Вэбсокет-соединение открыто');
+});
+
+socket.addEventListener('message', event => {
+    console.log('Принято сообщение');
+    // console.log(event);
+    try {
+        const msg = JSON.parse(event.data);
+        document.querySelector('.counter').textContent = msg.connections;
+        document.querySelector('.errors').value = msg.errors;
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (socket.bufferedAmount === 0) {
+        console.log(`socket.bufferedAmount = 0`);
+        socket.close(1000, 'just because');
+    }
+});
+
+socket.addEventListener('error', error => {
+    console.log(error.data);
+});
+
+socket.addEventListener('close', event => {
+    console.log('Вэбсокет соединение закрыто');
+    // console.log(event);
+});
+
