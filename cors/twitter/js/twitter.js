ad33cc7014705ef98ee12d090f75cb9f9a82ec57
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-03-07 21:30:06 +0300

    HJ 4.1 twitter is done

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -6,21 +6,27 @@ function randName() {
 
 function showLoadData(data) {
     console.log(data);
+    document.querySelector('[data-wallpaper]').src = data.wallpaper;
+    document.querySelector('[data-username]').textContent = data.username;
+    document.querySelector('[data-description]').textContent = data.description;
+    document.querySelector('[data-pic]').src = data.pic;
+    document.querySelector('[data-tweets]').value = data.tweets;
+    document.querySelector('[data-followers]').value = data.followers;
+    document.querySelector('[data-following]').value = data.following;
 }
 
-// const script = document.scripts[0].cloneNode();
+// const script = document.createElement('script');
 // script.src = `https://neto-api.herokuapp.com/twitter/jsonp?callback=showLoadData`;
 // document.body.appendChild(script);
 
 function loadData(url) {
     const callbackName = randName();
     return new Promise((done, fail) => {
-        // window[callbackName] = done;
-        window['rn123'] = done;
+        window[callbackName] = done;
 
-        const script = document.scripts[0].cloneNode();
-        // script.src = `${url}?callback=${callbackName}`;
-        script.src = `${url}?callback=rn123`;
+        // const script = document.scripts[0].cloneNode();
+        const script = document.createElement('script');
+        script.src = `${url}?callback=${callbackName}`;
         document.body.appendChild(script);
     });
 }
@@ -29,3 +35,10 @@ loadData('https://neto-api.herokuapp.com/twitter/jsonp')
     .then(showLoadData)
     .catch(err => console.log(err));
 
+// Вопрос:
+// Если строчку 
+// const script = document.createElement('script');
+// заменить на 
+// const script = document.scripts[0].cloneNode();
+// (как делают в лекции), то все перестает работать.
+// Почему??
