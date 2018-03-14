cc1fa271c95e1422322fb521224477328f213fb1
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-03-14 23:25:33 +0300

    HJ 4.4 socket: try to do. server does not work?

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+ws.addEventListener('error', error => {
+    console.log(error.data);
+});
+ws.addEventListener('close', event => {
+    console.log(`${event.code}: Вэбсокет соединение закрыто по причине ${event.reason}`);
+});
+ws.addEventListener('message', event => {
+    console.log(event.data);
+});
+
+ws.addEventListener('open', () => {
+    console.log('ws is opened');
+    window.editor.addEventListener('update', event => {
+        const canvas = event.canvas;
+        const ctx = canvas.getContext('2d');
+        const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const binary = Uint8Array.from(image.data);
+        ws.send(binary.buffer);
+        // console.log(binary);
+        // console.log(canvas);
+    });
+});
+
