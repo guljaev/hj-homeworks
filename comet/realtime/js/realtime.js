38168f3e3efda3982a83d4b4235190ec74f069ba
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-03-14 22:21:13 +0300

    HJ 4.4 realtime is done

--- comet/realtime/js/realtime.js
+++ comet/realtime/js/realtime.js
@@ -15,15 +15,23 @@ let isFirst = true;
 const ws = new WebSocket('wss://neto-api.herokuapp.com/realtime');
 ws.addEventListener('message', event => {
   if (isFirst) {
-    event.data
-      .split('\n')
-      .map(line => line.split('|'))
-      .forEach(data => realtime.addData([Number(data[1])], data[0]));
+    // console.log(event.data);
+    const data = JSON.parse(event.data);
+    data
+      .reverse()
+      .forEach(point => realtime.addData( [point.online], point.time) );
+    // event.data
+    //   .split('\n')
+    //   .map(line => line.split('|'))
+    //   .forEach(data => realtime.addData([Number(data[1])], data[0]));
 
     isFirst = false;
   } else {
-    const [label, data] = event.data.split('|');
+    // console.log(event.data);
+    // const [label, data] = event.data.split('|');
+    const point = JSON.parse(event.data);
     realtime.removeData();
-    realtime.addData([Number(data)], label);
+    realtime.addData([point.online], point.time);
+    // realtime.addData([Number(data)], label);
   }
 });
